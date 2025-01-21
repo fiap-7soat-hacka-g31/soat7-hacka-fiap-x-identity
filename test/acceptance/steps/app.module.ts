be75a80746bf362dde00aef01ba2b7d5b3eaa20e
gm50x <getuliomagela@outlook.com>
2025-01-21 20:20:16 -0300

    fix: adds missing test suite

--- test/acceptance/steps/app.module.ts
+++ test/acceptance/steps/app.module.ts
@@ -1,8 +1,9 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { SignInSuite } from './step-definitions/sign-in.suite';
 import { SignUpSuite } from './step-definitions/sign-up.suite';
 @Module({
   imports: [HttpModule],
-  providers: [SignUpSuite],
+  providers: [SignUpSuite, SignInSuite],
 })
 export class AppModule {}
