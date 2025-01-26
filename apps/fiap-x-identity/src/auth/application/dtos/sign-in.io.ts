2dcbde4fb401d1254b6c4263b1c8154bc28a80e9
gm50x <getuliomagela@outlook.com>
2025-01-26 13:02:06 -0300

    feat: maps api properties to swagger

--- apps/fiap-x-identity/src/auth/application/dtos/sign-in.io.ts
+++ apps/fiap-x-identity/src/auth/application/dtos/sign-in.io.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 import { AccessToken } from './token.dto';
 
 export class SignInInput {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   password: string;
 }
